08711555df2a080249017733196c7dde802b3b03
Jayant <jayantmeshram398@gmail.com>
2024-07-26 05:54:06 +0530

    streamlit update

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ from keras.models import load_model
 from helper import prepare_image_for_ela, prerpare_img_for_weather
 from fetchOriginal import image_coordinates, get_weather
 from PIL import Image as PILImage
+import io
 
 # Constants
 class_weather = ['Lightning', 'Rainy', 'Snow', 'Sunny']
@@ -35,33 +36,64 @@ def org_weather(img_name):
     return "The Image was taken at {} and weather there on {} was {}".format(location, date, weather)
 
 # Streamlit Interface
-st.title("Image Tampering Detection and Weather Classification")
+st.title("Image Tampering Detection Using ELA and Metadata Analysis")
 
-# Introductory Text
-st.markdown("""
-### Welcome to the Image Tampering Detector!
+st.markdown(""" 
+            ### Welcome to the Image Tampering Detector!
+            **What This Project Does:**
+            This tool helps you determine whether an image has been altered or manipulated. 
+            Using advanced technologies, I analyze images to reveal their authenticity.
+            """)
+with st.expander("How it Works?"):
+    st.markdown("""
+There are two parallels that are being used to identify an image's authenticity:
 
-**What This Project Does:**
-This tool helps you determine whether an image has been altered or manipulated. Using advanced technologies, we analyze images to reveal their authenticity.
-
-**How It Works:**
 1. **Error Level Analysis (ELA):**
-   - Images that are edited often show different compression artifacts compared to original ones. We use ELA to highlight these discrepancies, making tampering visible.
-   - **Real vs. Tampered Images:**
-    ![Real](rsc/ela.jpg)
-    ![Tampered Image](rsc/fake.jpg)
-         
-2. **Weather Validation:**
-   - By examining the metadata of the image, we check if the weather depicted matches historical data from the image’s recorded location and time.
-   
-
-**How To Use It:**
-1. **Upload an Image:** Choose an image to analyze.
-2. **Select the Outdoor Option:** Indicate whether the image is taken outdoors.
-3. **View Results:** Get insights into the image’s authenticity with detailed analysis.
-
-Try it out and see if your images hold up under scrutiny!
+   - Images that are edited often show different compression artifacts compared to original ones. I use ELA to highlight these discrepancies, making tampering visible.
 """)
+
+    # Create columns for horizontal layout
+    col1, col2 = st.columns(2)
+
+    # Display ELA images in columns
+    with col1:
+        st.image("rsc/real.jpg", caption="ELA of a Real Image", use_column_width=True)
+
+    with col2:
+        st.image("rsc/fake.jpg", caption="ELA of a Tampered Image", use_column_width=True)
+
+    st.markdown("""
+    Notice how ELA on real image shows consistency across the image but on fake image, ELA shows discrepancy across some regions.
+                
+    2. **Weather Validation:**
+        - **Purpose:** Weather Validation examines the metadata embedded in the image to cross-check if the depicted weather conditions are consistent with historical weather data from the image's recorded location and time.
+        - **How?:**
+            Images often contain metadata that includes information about the location, date, and time when the image was taken.
+            Using this metadata, historical weather databases are queried to retrieve the weather conditions for the recorded location and date.
+            Then actual weather conditions in the image are compared (as identified by a Weather CNN model) with the historical data to determine if they match.
+        - **Why It Matters:** This helps in verifying the authenticity of outdoor images by ensuring that the weather depicted matches what was recorded historically for that location and time. Discrepancies can indicate tampering or misrepresentation.
+""")
+with st.expander("How to use it?"):
+    st.markdown("""
+    1. **Upload an Image:** Choose an image to analyze.
+    2. **Select the Outdoor Option:** Indicate whether the image is taken outdoors.
+    3. **View Results:** Get insights into the image’s authenticity with detailed analysis.
+
+    Try it out and see if your images hold up under scrutiny!
+    """)
+
+with st.expander("Got a Query?"):
+    st.markdown("""
+Hi, this is Jayant
+                
+If you have any questions, feedback, suggestions, or just want to chat, feel free to reach out!
+
+- [LinkedIn Profile](https://www.linkedin.com/in/jayantmeshram/)
+- [GitHub Profile](https://github.com/jayant1211)
+
+you can also email me at jayantmeshram398@gmail.com.
+""")
+
 
 # Initialize session state
 if 'step' not in st.session_state:
@@ -96,9 +128,23 @@ if st.session_state.step > 0:
         st.write("3. " + res3)
 
     if st.button("Show Error Level Analysis") and st.session_state.step == 1:
+        # Display ELA result image
         st.image(ela_result, caption="ELA Analysis")
+
+        # Save ELA result image to a BytesIO object
+        buffer = io.BytesIO()
+        ela_result.save(buffer, format="JPEG")  # ela_result should be a PIL Image
+        buffer.seek(0)
+
+        # Add a download button for the ELA result image
+        st.download_button(
+            label="Save ELA Result Image",
+            data=buffer,
+            file_name="ela_result.jpg",
+            mime="image/jpeg"
+        )
 
     # Button to reset the session state and allow new image upload
     if st.button("Try New Image"):
         st.session_state.step = 0
-        st.experimental_rerun()
+        st.rerun()
